237210fc1b45f380a1692a9c9128301c50207fe9
Li Conglin <937423396@qq.com>
2023-07-10 23:59:38 +0800

    style:decode/DecodeLogic.scala注释

--- iFu/src/main/scala/backend/decode/DecodeLogic.scala
+++ iFu/src/main/scala/backend/decode/DecodeLogic.scala
@@ -5,26 +5,22 @@ import chisel3.util.BitPat
 import chisel3.util.experimental.decode._
 
 object DecodeLogic {
-    private def checkWidth(bp: BitPat, width: Int): BitPat = {
-        require(bp.width == width)
-        bp
-    }
 
     def apply(instr: UInt, default: BitPat, mapping: Iterable[(BitPat, BitPat)]): UInt =
         chisel3.util.experimental.decode.decoder(QMCMinimizer, instr, TruthTable(mapping, default))
 
     def apply(instr: UInt, default: Seq[BitPat], mapping: Iterable[(BitPat, Seq[BitPat])]): Seq[UInt] = {
-        val nSignals = default.size
+        val nSignals = default.size     //default中控制信号的数量
         require(mapping.forall(_._2.size == nSignals),
-            s"The number of signals doesn't match. default = $nSignals , mappingIn =  ${mapping.find(_._2.size != nSignals).get}"
+            s"The number of signals doesn't match. default = $nSignals , mapping =  ${mapping.find(_._2.size != nSignals).get}"
         )
-        val signalWidths = default.map(i => i.getWidth)
-        val totalWidth = signalWidths.sum
-        val signalIdxs = signalWidths.scan(totalWidth-1){
-            case(l,r) => l-r
-        }
-        val decoded = apply(instr, default.reduce(_ ## _), mapping.map { case (in, out) => (in, out.reduce(_ ## _)) })
+        val signalWidths = default.map(i => i.getWidth)     //每个控制信号的长度
+        val totalWidth = signalWidths.sum                   //控制信号的总长度
+        //每个控制信号的索引
+        val signalIdxs = signalWidths.scan(totalWidth-1){ (l,r) => l-r}
         //返回译码结果
+        val decoded = apply(instr, default.reduce(_ ## _), mapping.map { case (inst, signals) => (inst, signals.reduce(_ ## _)) })
+            //将译码结果转换为Seq[BitPat]
         signalIdxs.zip(signalIdxs.tail).map { case (lidx, ridx) => decoded(lidx, ridx + 1) }.toList
     }
 }
