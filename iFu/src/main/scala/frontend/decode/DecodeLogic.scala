71df441d51033772101b7021add78857979e2948
Li Conglin <937423396@qq.com>
2023-07-10 22:51:11 +0800

    Not fixed

--- iFu/src/main/scala/frontend/decode/DecodeLogic.scala
+++ iFu/src/main/scala/frontend/decode/DecodeLogic.scala
@@ -5,7 +5,15 @@ import chisel3.util.BitPat
 import chisel3.util.experimental.decode._
 
 object DecodeLogic {
-    // TODO
+    private def checkWidth(bp: BitPat, width: Int): BitPat = {
+        if (bp.width == width) bp
+        else {
+            require(bp.width == width, s"Not aligned bp = '$bp',width = '$width'")
+            val diff = width - bp.width
+            require(diff > 0, s"Cannot pad '$bp' to '$width' because it is already '${bp.width}' bits wide!")
+            BitPat(0.U(diff.W)) ## bp
+        }
+    }
     def apply(addr: UInt, default: BitPat, mapping: Iterable[(BitPat, BitPat)]): UInt =
       chisel3.util.experimental.decode.decoder(QMCMinimizer, addr, TruthTable(mapping, default))
 
@@ -14,7 +22,7 @@ object DecodeLogic {
         require(mappingIn.forall(_._2.size == nElts),
             s"All Seq[BitPat] must be of the same length, got $nElts vs. ${mappingIn.find(_._2.size != nElts).get}"
         )
-        //思路：将default转化为BitPat的形式，然后调用前面的apply函数来生成decoder
+
         val elementsGrouped = mappingIn.map(_._2).transpose
         val elementWidths = elementsGrouped.zip(default).map { case (elts, default) =>
             (default :: elts.toList).map(_.getWidth).max //将default添加到列表的开头，最终得到一行元素中最大宽度，即每一个信号对应的宽度
@@ -23,11 +31,11 @@ object DecodeLogic {
 
         val elementIndices = elementWidths.scan(resultWidth - 1) { case (l, r) => l - r }
 
-        val defaultsPadded = default.zip(elementWidths).map { case (bp, w) => padBP(bp, w) }
+        val defaultsPadded = default.zip(elementWidths).map { case (bp, w) => checkWidth(bp, w) }
         val mappingInPadded = mappingIn.map { case (in, elts) =>
-            in -> elts.zip(elementWidths).map { case (bp, w) => padBP(bp, w) }
+            in -> elts.zip(elementWidths).map { case (bp, w) => checkWidth(bp, w) }
         }
-        val decoded = apply(addr, default.reduce(_ ## _), mappingIn.map { case (in, out) => (in, out.reduce(_ ## _)) })
+        val decoded = apply(addr, defaultsPadded.reduce(_ ## _), mappingInPadded.map { case (in, out) => (in, out.reduce(_ ## _)) })
         //返回译码结果
         elementIndices.zip(elementIndices.tail).map { case (msb, lsb) => decoded(msb, lsb + 1) }.toList
     }
