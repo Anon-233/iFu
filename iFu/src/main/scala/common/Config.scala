45bec160fe34796e0bd904a0a35cc00ba406f69f
xiao <3012263655@qq.com>
2023-07-11 14:40:53 +0800

    feat：新增Config

--- iFu/src/main/scala/common/Config.scala
+++ iFu/src/main/scala/common/Config.scala
@@ -5,3 +5,11 @@ import chisel3._
 final case class FrontendConfig(
     fetchWidth: Int = 8
 )
+
+final case class iFuConfig(
+    frontend: FrontendConfig = FrontendConfig()
+)
+
+object iFuConfig {
+    def normal = iFuConfig()
+}
