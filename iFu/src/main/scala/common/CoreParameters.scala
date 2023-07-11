2efe465a02b1d99ffbf9ef36d3f6f653c5860a58
xiao <3012263655@qq.com>
2023-07-11 16:36:23 +0800

    feat：新增uop

--- iFu/src/main/scala/common/CoreParameters.scala
+++ iFu/src/main/scala/common/CoreParameters.scala
@@ -6,13 +6,20 @@ final case class BPUParameters(
     // TODO: add more parameters
 )
 
+final case class ICacheParameters(
+    blockBytes: Int = 256,
+    nBanks: Int = 2
+)
+
 final case class FrontendParameters(
-    // TODO: add more parameters
+    iCacheParams: ICacheParameters = ICacheParameters()
+    numFTQEntries: Int = 40
 )
 
 trait HasCoreParameters {
-    val vaddrBits: Int = 32     // 虚拟地址位数
-    val coreInstrBytes: Int = 4 // 指令字节数
-    val frontendParams: FrontendParameters = FrontendParameters()
+    val vaddrBits: Int = 32                     // 虚拟地址位数
+    val coreInstrBytes: Int = 4                 // 指令字节数
+    val coreInstrBits: Int = coreInstrBytes * 8 // 指令位数
     val bpdParams: BPUParameters = BPUParameters()
+    val frontendParams: FrontendParameters = FrontendParameters()
 }
--- iFu/src/main/scala/common/MicroOp.scala
+++ iFu/src/main/scala/common/MicroOp.scala
@@ -3,6 +3,11 @@ package iFu.common
 import chisel3._
 import chisel3.util._
 
-class MicroOp extends CoreBundle {
-
+class MicroOp extends CoreBundle with MicroOpCode {
+    val pcLowBits = UInt(log2Ceil(frontendParams.iCacheParams.blockBytes).W)
+    val instr = UInt(coreInstrBits.W)
+    val uopc = UInt(UOPC_SZ.W)
+    val ftqIdx = UInt(log2Ceil(frontendParams.numFTQEntries).W)
+    val taken = Bool()
+    val isSFB = Bool()
 }
