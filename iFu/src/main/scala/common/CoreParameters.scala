a8941c7c37c061be5af43440af9ff84ff61219e5
xiao <3012263655@qq.com>
2023-07-11 15:18:11 +0800

    feat：新增参数

--- iFu/src/main/scala/common/CoreParameters.scala
+++ iFu/src/main/scala/common/CoreParameters.scala
@@ -2,7 +2,14 @@ package iFu.common
 
 import chisel3._
 
+final case class BPUParameters(
+    // TODO: add more parameters
+)
+
 trait HasCoreParameters {
+    val vaddrBits: Int = 32     // 虚拟地址位数
+    val coreInstrBytes: Int = 4 // 指令字节数
     val fetchWidth: Int = 8     // 一次取值宽度
     val nBanks: Int = 2
+    val bpdParams: BPUParameters = BPUParameters()
 }
