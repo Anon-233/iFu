7a668b374e7c463a447ff8905de39c6e82d81957
ZeroKnighting <1812234264@qq.com>
2023-07-13 17:54:40 +0800

    fear: add freelist,maptable(wait to debug)

---
+++ iFu/src/main/scala/backend/rename/FreeList.scala
@@ -0,0 +1,70 @@
+package iFu.backend
+
+import chisel3._
+import chisel3.util._
+import iFu.common._
+
+class FreeList(
+    val plWidth: Int,
+    val numPregs: Int,
+    val numLregs: Int)(implicit p:parameters) extends CoreBundle
+{
+    val pregSize = log2Ceil(numPregs)
+    val io =IO(new CoreBundle()(p){
+        val reqs = Input(Vec(plWidth,Bool()))
+        val alloc_pregs = Output(Vec(plWidth,Valid(UInt(pregSize.W))))
+
+        //ROB返回
+        val dealloc_pregs = Input(Vec(plWidth,Valid(UInt(pregSize.W))))
+
+        val ren_br_tags   = Input(Vec(plWidth, Valid(UInt(brTagSz.W))))
+
+        val brupdate = Input(new BrUpdateInfo)
+
+    })
+
+    //除了x0其他的寄存器刚开始都是free
+    val freeList = RegInit(UInt(numPregs.W),~(1.U(numPregs.W)))
+    val brAllocList = Reg(Vec(maxBrCount,UInt(numPregs.W)))
+
+    //分配
+    val selPreg = SelectFirstN(free_list,plWidth)
+    val selPregFire = Wire(Vec(plWidth,Bool()))
+
+    val allocPreg = io.alloc_pregs map (a => UIntToOH(a.bits))
+    val allocPregMask = (allocPreg zip io.reqs).scanRight(0.U(n.W)){ case ((a,r),m) => m | a & Fill(n,r) }
+
+    //标记分配mask
+    val sleMask = (selPreg zip selPregFire) map { case (s,f) => s & Fill(numPregs,f) } reduce(_|_)
+    //预测错误的分支中空闲物理寄存器的独热码集合
+    val brDeallocs = brAllocList(io.brupdate.b2.uop.br_tag) & Fill(numPregs,io.brupdate.b2.mispredict)
+    //需要释放的物理寄存器独热码集合
+    val deallocMask = io.dealloc_pregs.map(d => UIntToOH(d.bits)(numPregs-1,0) & Fill(numPregs,d.valid)).reduce(_|_) | brDeallocs
+
+    val brSlots = VecInit(io.ren_br_tags.map(tag => tag.valid)).asUInt
+
+    //分支指令分配列表
+    for (i <-0 until maxBrCount){
+        val listReq = VecInit(io.ren_br_tags.map(tag => UIntToOH(tag.bits)(i))).asUInt & brSlots
+        val newList = listReq.orR
+        brAllocList(i) := Mux(newList,Mux1H(listReq,allocPregMask.slice(1,plWidth+1)),
+                            brAllocList(i) & ~brDeallocs | allocPregMask(0))
+    }
+
+    //更新
+    freeList := (freeList & ~selMask | deallocMask) & ~(1.U(numpregs.W))
+
+
+    //输出
+    for(w <- 0 until plWidth){
+        val can = selPreg(w).orR
+        val rValid = RegInit(false.B)
+        val rSel = RegEnable(OHToUInt(selPreg(w)),selPregFire(w))
+
+        rValid := rValid & !io.reqs(w) ||can
+        selPregFire(w) := (!rValid || io.reqs(w)) && can
+
+        io.alloc_pregs(w).bits := rSel
+        io.alloc_pregs(w).valid := rValid
+    }
+}   
---
+++ iFu/src/main/scala/backend/rename/MapTable.scala
@@ -0,0 +1,91 @@
+package iFu.backend
+
+import chisel3._
+import chisel3.util._
+import iFu.common._
+
+class MaptableReq(val lregSz:Int) extends Bundle
+{
+    val lrs1 = UInt(lregSz.W)
+    val lrs2 = UInt(lregSz.W)
+    val ldst = UInt(lregSz.W)
+}
+
+class MaptableResp(val pregSz:Int) extends Bundle
+{
+    val prs1 = UInt(pregSz.W)
+    val prs2 = UInt(pregSz.W)
+    val stale_pdst = UInt(pregSz.W)
+}
+
+class ReMapReq(val lregSz:Int,val pregSz:Int) extends Bundle
+{
+    val ldst = UInt(lregSz.W)
+    val pdst = UInt(pregSz.W)
+    val valid = Bool()
+}
+
+class RenamemapTable(
+    val plWidth:Int,
+    val numLregs:Int,
+    val numPregs:Int)(implicit p: Parameters) extends CoreBundle
+{
+    val pregSize = log2Ceil(numPregs)
+
+    val io = IO(new CoreBundle()(p){
+        val map_reqs = Input(Vec(plWidth,new MaptableReq(lregSz)))
+        val map_resps = Output(Vec(plWidth,new MaptableResp(pregSz)))
+
+        val remap_reqs = Input(Vec(plWidth,new ReMapReq(lregSz,pregSz)))
+
+        val ren_br_tags = Input(Vec(plWidth,Valid(UInt(brTagSz.W))))
+
+        val brupdate = Input(new BrUpdateInfo)
+        val rollback = Input(Bool())
+    })
+
+    val mapTable = RegInit(VecInit(Seq.fill(numLregs){0.U(pregSize.W)}))
+    val brShotMap = Reg(Vec(maxBrCount, Vec(numLregs, UInt(pregSize.W))))
+
+    val remapTable = Wire(Vec(plWidth+1,Vec(numLregs,UInt(pregSize.W))))
+
+    val remapPdsts = io.remap_reqs map (_.pdst)
+    val remap_ldsts_oh = io.remap_reqs (req => UIntToOH(req.ldst) & Fill(numLregs,req.valid.asUInt))
+
+    //更新映射关系
+    for(i <- 0 until numLregs){ //遍历每一个寄存器
+        if(i == 0){
+            for(j <- 0 until plWidth+1){
+                remapTable(j)(i) := 0.U //寄存器x0固定为0
+            }
+        }else{
+            val row = (remap_ldsts_oh.map(ldst => ldst(i)) zip remapPdsts)
+                .scanLeft(mapTable(i)){case (pdst,(ldst,new_pdst)) => Mux(ldst,new_pdst,pdst)}
+
+            for(j <- 0 until plWidth+1){
+                remapTable(j)(i) := row(j)
+            }
+        }
+    }
+
+    //保存分支的map关系（分支预测错误需要回滚)
+    for(i <- 0 until plWidth){
+        when(io.ren_br_tags(i).valid){
+            br_snapshots(io.ren_br_tags(i).bits) := remapTable(i+1) //i+1是因为scanleft函数
+        }
+    }
+
+    //更新maptable
+    when(io.brupdate.b2.mispredict){
+        mapTable := brShotMap(io.brupdate.b2.uop.br_tag)
+    } .otherwise {
+        mapTable := remapTable(plWidth)
+    }
+
+    //输出,对于转发在顶层模块判断和进行
+      for (i <- 0 until plWidth) {
+        io.map_resps(i).prs1       := (0 until i).foldLeft(map_table(io.map_reqs(i).lrs1)) ((p,k) =>p)
+        io.map_resps(i).prs2       := (0 until i).foldLeft(map_table(io.map_reqs(i).lrs2)) ((p,k) =>p)
+        io.map_resps(i).stale_pdst := (0 until i).foldLeft(map_table(io.map_reqs(i).ldst)) ((p,k) =>p)
+  }
+}
