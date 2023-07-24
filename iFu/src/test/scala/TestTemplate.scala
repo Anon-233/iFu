2fa918e423a7072ef35a380c2686721403c00b7b
xiao <3012263655@qq.com>
2023-07-24 22:05:37 +0800

    feat: 修改测试文件

--- iFu/src/test/scala/TestTemplate.scala
+++ iFu/src/test/scala/TestTemplate.scala
@@ -2,7 +2,7 @@ import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
-class SimpleTestExpect extends AnyFlatSpec with ChiselScalatestTester {
+class TestTemplate extends AnyFlatSpec with ChiselScalatestTester {
     "DUT" should "pass" in {
         test(new DeviceUnderTest) { dut =>
             println("Testing DUT")
