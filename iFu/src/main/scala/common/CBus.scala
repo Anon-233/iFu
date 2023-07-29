737152031d6ae438c2b718d01bc994e238062aec
xiao <3012263655@qq.com>
2023-07-30 02:18:22 +0800

    feat: cbus

--- iFu/src/main/scala/common/CBus.scala
+++ iFu/src/main/scala/common/CBus.scala
@@ -3,22 +3,36 @@ package iFu.common
 import chisel3._
 import chisel3.util._
 
+import iFu.common.Consts._
 
-
-
-class CbusReq extends CoreBundle{
+class CBusReq(val s: Int) extends CoreBundle {
     val valid = Bool()
     val isStore = Bool()
-    val size = UInt(3.W)
-    val addr = UInt(32.W)
-    val data = UInt(32.W)
+    val size = UInt(MSIZE1.getWidth.W)
+    val addr = UInt(paddrBits.W)
+    val data = UInt((1 << (s + 3)).W)
     val mask = UInt((nRowBytes).W)//两个cache这里的AXI可以不同
-    val axiBurstType = UInt(2.W)
-    val axiLen = UInt(8.W)
+    val axiBurstType = UInt(AXI_BURST_INCR.getWidth.W)
+    val axiLen = UInt(MLEN1.getWidth.W)
 }
 
-class CbusResp extends CoreBundle{
-    val data = UInt(32.W)
+class CBusResp(val s: Int) extends CoreBundle{
+    val data = UInt((1 << (s + 3)).W)
     val isLast = Bool()
     val ready = Bool()
 }
+
+class iCBusReq extends CBusReq(2) {
+    val size = MSIZE4
+    val axiBurstType = AXI_BURST_INCR
+    val axiLen = MLEN16
+}
+
+class dCBusReq extends CBusReq(2) {
+    val size = MSIZE4
+    val axiBurstType = AXI_BURST_INCR
+    val axiLen = MLEN16
+}
+
+class iCBusResp extends CBusResp(2)
+class dCBusResp extends CBusResp(2)
