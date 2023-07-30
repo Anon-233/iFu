e23d1c38f04b88fe52c5838f8408719229d39042
Li Conglin <937423396@qq.com>
2023-07-30 22:45:00 +0800

    style:NullMicroOp

---
+++ iFu/src/main/scala/backend/memSystem/DTlb.scala
@@ -0,0 +1,26 @@
+package backend.memSystem
+
+import chisel3._
+import chisel3.util._
+import iFu.common._
+import iFu.frontend.{SFenceReq, TLBReq, TLBResp}
+
+class DTlb extends CoreModule{
+    val io = IO(new Bundle {
+        val req = Flipped(Vec(memWidth, Decoupled(new TLBReq)))
+        val miss_rdy = Output(Bool())
+        val resp = Output(Vec(memWidth, new TLBResp))
+        val sfence = Input(Valid(new SFenceReq))
+        val kill = Input(Bool())
+    })
+    for(w <- 0 until memWidth){
+        io.resp(w).miss := false.B
+        io.resp(w).paddr:= io.req(w).bits.vaddr
+        io.resp(w).pf   := false.B
+        io.resp(w).gf   := false.B
+        io.resp(w).ae   := false.B
+        io.resp(w).ma   := false.B
+        io.req(w).ready := true.B
+
+    }
+}
