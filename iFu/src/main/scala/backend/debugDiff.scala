c5ccffa64dbe19c80ea79b09fddec0632eec8f9a
ZeroKnighting <1812234264@qq.com>
2023-08-01 00:07:56 +0800

    增加判断pc2

--- iFu/src/main/scala/backend/debugDiff.scala
+++ iFu/src/main/scala/backend/debugDiff.scala
@@ -16,9 +16,12 @@ class debugDiff extends CoreModule {
   for(w <- 0 until coreWidth){
     val lreg = io.commit.debug_ldst(w)  //这个接目的寄存器
     val wdata = io.commit.debug_wdata(w)  //这个接写入数据
-    val nowPc = io.commit.debug_pc(w)  //这个接pc
     val nowInst = io.commit.debug_insts(w) //当前指令
     val valid = nowInst =/= BUBBLE //判断指令是否有效
+    val nowPc = io.commit.debug_pc(w)  //这个接pc
+    val nowPc2 = io.commit.uops(w).debug_pc //这个和上面那个应该要相同
+    //require((nowPc == nowPc2) && valid)  //如果不同，说明rob存在问题
+
 
     debug_reg(lreg) := wdata
   }
