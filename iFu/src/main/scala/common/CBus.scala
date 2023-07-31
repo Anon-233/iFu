7084ed72d8281c1149fe79cac0c75a20a23ce4cd
liusiyuan <louis.ryant2022@gmail.com>
2023-08-01 02:58:44 +0800

    cbus

--- iFu/src/main/scala/common/CBus.scala
+++ iFu/src/main/scala/common/CBus.scala
@@ -5,17 +5,8 @@ import chisel3.util._
 
 import iFu.common.Consts._
 
-class CBusReq(val s: UInt = MSIZE4) extends CoreBundle {
-    val dataWidth = s match {
-        case MSIZE1   => 8
-        case MSIZE2   => 16
-        case MSIZE4   => 32
-        case MSIZE8   => 64
-        case MSIZE16  => 128
-        case MSIZE32  => 256
-        case MSIZE64  => 512
-        case MSIZE128 => 1024
-    }
+class CBusReq extends CoreBundle {
+    val dataWidth = 32
 
     val valid = Bool()
     val isStore = Bool()
@@ -27,17 +18,8 @@ class CBusReq(val s: UInt = MSIZE4) extends CoreBundle {
     val axiLen = UInt(MLEN1.getWidth.W)
 }
 
-class CBusResp(val s: UInt = MSIZE4) extends CoreBundle{
-    val dataWidth = s match {
-        case MSIZE1   => 8
-        case MSIZE2   => 16
-        case MSIZE4   => 32
-        case MSIZE8   => 64
-        case MSIZE16  => 128
-        case MSIZE32  => 256
-        case MSIZE64  => 512
-        case MSIZE128 => 1024
-    }
+class CBusResp extends CoreBundle{
+    val dataWidth = 32
     
     val data = UInt(dataWidth.W)
     val isLast = Bool()
