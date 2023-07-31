d550436d395f1e678a3ab3fed4395054da5c07e3
ZeroKnighting <1812234264@qq.com>
2023-07-31 23:59:57 +0800

    修正diff

--- iFu/src/main/scala/backend/debugDiff.scala
+++ iFu/src/main/scala/backend/debugDiff.scala
@@ -11,16 +11,20 @@ class debugDiff extends CoreModule {
   })
 
   val debug_reg = Mem(lregSz,UInt(xLen.W))
-  val inst = io.commit.debug_insts  //这个接pc，注意判断是否为bubble
+
 
   for(w <- 0 until coreWidth){
-    val lreg = io.commit.debug_ldst  //这个接目的寄存器
-    val wdata = io.commit.debug_wdata  //这个接写入数据
+    val lreg = io.commit.debug_ldst(w)  //这个接目的寄存器
+    val wdata = io.commit.debug_wdata(w)  //这个接写入数据
+    val nowPc = io.commit.debug_pc(w)  //这个接pc
+    val nowInst = io.commit.debug_insts(w) //当前指令
+    val valid = nowInst =/= BUBBLE //判断指令是否有效
 
     debug_reg(lreg) := wdata
   }
   for(w <-0 until lregSz){
-    io.lregOut(w) := debug_reg(w) //接寄存器堆
+    val regRead = debug_reg(w)   //这个接寄存器堆
+    io.lregOut(w) := regRead //接寄存器堆
   }
 
 
