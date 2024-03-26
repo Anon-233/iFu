767f0e033ed7d4ce3cdcc60b97a33a1eed6a31f2
xiao <3012263655@qq.com>
2024-03-26 18:46:13 +0800

    bugfix: DTLB dmw1_en

--- iFu/src/main/scala/tlb/DTLB.scala
+++ iFu/src/main/scala/tlb/DTLB.scala
@@ -38,7 +38,7 @@ class DTLBReq extends CoreBundle(){
     val size = UInt(2.W)
 
     val is_tlb_inst = Bool()
-//    val cmd = Bits(TLB_CMD_SZ.W)
+    // val cmd = Bits(TLB_CMD_SZ.W)
 }
 class DTLBResp extends CoreBundle(){
     val paddr               = UInt(paddrBits.W)
@@ -51,7 +51,7 @@ class DTLBIO extends CoreBundle(){
     val r_req       = Vec(memWidth, Decoupled(new TLBDataRReq))
     val r_resp      = Vec(memWidth, Flipped(Valid(new TLBDataRResp)))
     val w_req       = Decoupled(new TLBDataWReq)
-//    val w_resp = Input(Valid(new WResp))
+    // val w_resp = Input(Valid(new WResp))
     val dtlb_csr_context = Input(new DTLBCsrContext)
     val tlb_data_context = Input(new TLBDataCsrContext)
     val kill = Input(Bool())
@@ -61,11 +61,11 @@ class DTLBIO extends CoreBundle(){
 class DTLB extends CoreModule(){
     val io = IO(new DTLBIO)
     io <> DontCare  //TODO 删除
-//    when(io.req(0).bits.is_tlb_inst){
-//        io.w_req.valid      := true.B
-//        io.w_req.bits.vaddr := io.req(0).bits.vaddr
-//        io.w_req.bits.cmd   := io.req(0).bits.cmd
-//    }
+    // when(io.req(0).bits.is_tlb_inst){
+    //     io.w_req.valid      := true.B
+    //     io.w_req.bits.vaddr := io.req(0).bits.vaddr
+    //     io.w_req.bits.cmd   := io.req(0).bits.cmd
+    // }
     for(w <- 0 until memWidth){
         io.resp(w)          := 0.U.asTypeOf(new DTLBResp)
         io.resp(w).paddr    := io.req(w).bits.vaddr
@@ -80,65 +80,68 @@ class DTLB extends CoreModule(){
             }/*.elsewhen(*/
         }
 
-//        io.r_req(w).valid := true.B
-//        io.r_req(w).bits.addr := io.req(w).bits.vaddr
+        // io.r_req(w).valid := true.B
+        // io.r_req(w).bits.addr := io.req(w).bits.vaddr
 
-//        when(!io.r_resp(w).valid){
-//            io.resp(w).exceptions.valid         := true.B
-//            io.resp(w).exceptions.bits.is_tlbr  := true.B
-//
-//        } .elsewhen (io.r_resp(w).valid){
-//            val meta = io.r_resp(w).bits.meta
-//            when(meta.v){
-//                when(io.req(w).bits.use_ldq === true.B) {
-//                    io.resp(w).exceptions.bits.is_pil := true.B
-//                }.elsewhen(io.req(w).bits.use_stq === true.B) {
-//                    io.resp(w).exceptions.bits.is_pis := true.B
-//                }
-//            } .elsewhen(io.csr_context.plv < meta.plv) {
-//                io.resp(w).exceptions.valid         := true.B
-//                io.resp(w).exceptions.bits.is_ppi   := true.B
-//            } .elsewhen(io.req(w).bits.use_stq && !meta.D) {
-//                io.resp(w).exceptions.valid         := true.B
-//                io.resp(w).exceptions.bits.is_pme   := true.B
-//            }
-//        }
+        // when(!io.r_resp(w).valid){
+        //     io.resp(w).exceptions.valid         := true.B
+        //     io.resp(w).exceptions.bits.is_tlbr  := true.B
 
+        // } .elsewhen (io.r_resp(w).valid){
+        //     val meta = io.r_resp(w).bits.meta
+        //     when(meta.v){
+        //         when(io.req(w).bits.use_ldq === true.B) {
+        //             io.resp(w).exceptions.bits.is_pil := true.B
+        //         }.elsewhen(io.req(w).bits.use_stq === true.B) {
+        //             io.resp(w).exceptions.bits.is_pis := true.B
+        //         }
+        //     } .elsewhen(io.csr_context.plv < meta.plv) {
+        //         io.resp(w).exceptions.valid         := true.B
+        //         io.resp(w).exceptions.bits.is_ppi   := true.B
+        //     } .elsewhen(io.req(w).bits.use_stq && !meta.D) {
+        //         io.resp(w).exceptions.valid         := true.B
+        //         io.resp(w).exceptions.bits.is_pme   := true.B
+        //     }
+        // }
     }
-//    val data_uncache_en = Bool()
-//    data_uncache_en := (da_mode && (csr_datm === 0.U)) ||
-//        (dmw0_en && (csr_dmw0(`DMW_MAT) === 0.U)) ||
-//        (dmw1_en && (csr_dmw1(`DMW_MAT) === 0.U)) ||
-//        (data_addr_trans_en && (data_tlb_mat === 0.U))
+    // val data_uncache_en = Bool()
+    // data_uncache_en := (da_mode && (csr_datm === 0.U)) ||
+    //     (dmw0_en && (csr_dmw0(`DMW_MAT) === 0.U)) ||
+    //     (dmw1_en && (csr_dmw1(`DMW_MAT) === 0.U)) ||
+    //     (data_addr_trans_en && (data_tlb_mat === 0.U))
 
-//    for(w <- 0 until memWidth) {
-//        io.r_req(w).valid := io.req(w).valid
-//        io.r_req(w).bits.vaddr := io.req(w).bits.vaddr
-//        io.r_req(w).bits.cmd :=
-//    }
+    // for(w <- 0 until memWidth) {
+    //     io.r_req(w).valid := io.req(w).valid
+    //     io.r_req(w).bits.vaddr := io.req(w).bits.vaddr
+    //     io.r_req(w).bits.cmd :=
+    // }
 
     //TODO disable_cache是一个寄存器,以及data_addr_trans_en的支持
     val csr_regs = io.dtlb_csr_context
     val da_mode = csr_regs.crmd_da && !csr_regs.crmd_pg
     val pg_mode = !csr_regs.crmd_da && csr_regs.crmd_pg
-    for (w <- 0 until memWidth){
+    for (w <- 0 until memWidth) {
+        val dmw0_en = (
+            (
+                (csr_regs.dmw0_plv0 && csr_regs.crmd_plv === 0.U) ||
+                (csr_regs.dmw0_plv3 && csr_regs.crmd_plv === 3.U)
+            ) &&
+            (io.req(w).bits.vaddr(31, 29) === csr_regs.dmw0_vseg) &&
+            pg_mode
+        )
+        val dmw1_en = (
+            (
+                (csr_regs.dmw1_plv0 && csr_regs.crmd_plv === 0.U) ||
+                (csr_regs.dmw1_plv3 && csr_regs.crmd_plv === 3.U)
+            ) &&
+            (io.req(w).bits.vaddr(31, 29) === csr_regs.dmw1_vseg) &&
+            pg_mode
+        )
 
-        val dmw0_en = ((csr_regs.dmw0_plv0 && csr_regs.crmd_plv === 0.U) ||
-          (csr_regs.dmw0_plv3 && csr_regs.crmd_plv === 3.U)) &&
-          (io.req(w).bits.vaddr(31, 29) === csr_regs.dmw0_vseg) &&
-          pg_mode
-
-        val dmw1_en = ((csr_regs.dmw1_mat(0) && csr_regs.crmd_plv === 0.U) ||
-          (csr_regs.dmw1_plv3 && csr_regs.crmd_plv === 3.U)) &&
-          (io.req(w).bits.vaddr(31, 29) === csr_regs.dmw1_vseg) &&
-          pg_mode
-
-        io.resp(w).is_uncacheable := (da_mode && csr_regs.crmd_datm === 0.U) ||
-          (dmw0_en && (csr_regs.dmw0_mat === 0.U)) ||
-          (dmw1_en && (csr_regs.dmw1_mat === 0.U))
-
+        io.resp(w).is_uncacheable := (
+            (da_mode && (csr_regs.crmd_datm === 0.U)) ||
+            (dmw0_en && (csr_regs.dmw0_mat === 0.U))  ||
+            (dmw1_en && (csr_regs.dmw1_mat === 0.U))
+        )
     }
 }
-
-
-
